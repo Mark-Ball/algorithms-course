f8ed64efa9324a53f9d968ec21f93ce04bb2974c
Mark-Ball <markball111@gmail.com>
2020-05-12 12:31:14 +1000

    implemented factorial recursive function

---
+++ factorial.js
@@ -0,0 +1,16 @@
+// recursive function
+// accepts a number and returns the factorial of that number
+
+function factorial(n) {
+    // base case
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040
