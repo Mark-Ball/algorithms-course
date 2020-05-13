eb419f3211d12a791f0d1ae7909a79ebd24ff852
Mark-Ball <markball111@gmail.com>
2020-05-13 10:32:23 +1000

    implemented factorial iteratively

--- factorial.js
+++ factorial.js
@@ -1,6 +1,15 @@
 // recursive function
 // accepts a number and returns the factorial of that number
 
+function iterativeFactorial(n) {
+    let result = 1;
+    while (n > 0) {
+        result *= n;
+        n -= 1;
+    }
+    return result;
+}
+
 function factorial(n) {
     // base case
     if (n <= 1) {
@@ -14,3 +23,4 @@ console.log(factorial(1)); // 1
 console.log(factorial(2)); // 2
 console.log(factorial(4)); // 24
 console.log(factorial(7)); // 5040
+console.log(iterativeFactorial(7)); // 5040
