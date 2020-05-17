1c60876908a00fe8b6fba4c72af71ce0a7a013d6
Mark-Ball <markball111@gmail.com>
2020-05-17 12:19:44 +1000

    implemented naive string search

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -21,4 +21,4 @@ function binarySearch(arr, val) {
     return -1;
 }
 
-console.log(binarySearch([1, 3, 4, 6, 8, 9, 11, 12, 15, 16, 17, 18, 19], 20)); // -1
+console.log(binarySearch([1, 3, 4, 6, 8, 9, 11, 12, 15, 16, 17, 18, 19], 19)); // -1
--- searching/naiveStringSearch.js
+++ searching/naiveStringSearch.js
@@ -2,19 +2,29 @@
 
 function stringSearch(bigString, littleString) {
     let counter = 0;
+    let matchBegin = 0;
     for (let i = 0; i < bigString.length; i++) {
         for (let j = 0; j < littleString.length; j++) {
-            if (j === littleString.length - 1) {
-                counter++;
+            console.log(bigString[i], littleString[j]);
+            if (bigString[i] === littleString[j]) {
+                if (j === 0) {
+                    matchBegin = i; // need to remember where the match began so we can walk back
+                }
+                if (j === littleString.length - 1) {
+                    counter++;
+                    break;
+                }
+                i++;
+            } else {
+                if (j > 0) {
+                    i = matchBegin;
+                }
                 break;
             }
-            if (bigString[i] !== littleString[j]) {
-                break;
-            }
-            i++;
+            
         }
     }
     return counter;
 }
 
-console.log(stringSearch('zomgomgomg', 'omg')); // 3
+console.log(stringSearch('oomg', 'omg')); // 3
